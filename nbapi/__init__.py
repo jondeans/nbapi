d5b8e7de33dff7a20c38dc6b8836c8b3bee5e5f6
jondeans <jrdeans@gmail.com>
2021-03-14 10:03:13 -0700

    Set up requests-cache.

--- nbapi/__init__.py
+++ nbapi/__init__.py
@@ -1,1 +1,15 @@
 """"""
+
+from datetime import timedelta
+
+CACHE_EXPIRE_MINUTES = 10
+
+# Set up requests-cache, if available
+HAS_REQUESTS_CACHE = True
+try:
+    from requests_cache import install_cache
+
+    install_cache("nbapi_cache", expire_after=timedelta(minutes=CACHE_EXPIRE_MINUTES))
+except ModuleNotFoundError as err:
+    HAS_REQUESTS_CACHE = False
+    print(err)
